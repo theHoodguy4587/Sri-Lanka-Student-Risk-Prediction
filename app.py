a4424cdc75979c68aa51a5e17f436794e88e75fa
theHoodguy4587 <senitha02@gmail.com>
2025-12-26 11:27:53 +0530

    app.py

--- app.py
+++ app.py
@@ -2,17 +2,20 @@ import streamlit as st
 import pickle
 import pandas as pd
 
-model=pickle.load(open("models/stu_risk_model.pkl","rb"))
-features=pickle.load(open("models/stu_features.pkl","rb"))
+model=pickle.load(open("E:/Student performance/models/student_risk_model.pkl","rb"))
+features=pickle.load(open("E:/Student performance/models/features.pkl","rb"))
 
 st.title("Student Academic Risk Prediction System (Sri Lanka – Prototype)")
-st.write("This application predicts whether a student is academically **at risk** ""based on mid-term performance and related factors.")
+st.write("This application estimates whether a student may be academically at risk based on prior academic indicators and related behavioural and family factors.It is intended as an early warning tool to support academic monitoring,not as a prediction of final examination results.")
 
 st.markdown(
     """
-    **Context:**  
-    This is a mid-term academic risk monitoring system.  
-    Term test scores represent continuous assessments commonly used in Sri Lankan schools.
+   **Context**:
+This system uses historical academic indicators and contextual factors
+to identify students who may require additional academic support.
+
+It does not require users to enter term test or final examination marks.
+
     """
 )
 st.subheader("Student Information")
@@ -20,8 +23,21 @@ st.subheader("Student Information")
 sex_label=st.selectbox("Gender",["Male","Female"])
 age=st.number_input("Age",min_value=10,max_value=25,value=16)
 
+medu=st.selectbox("Mother's educatonal level",  [
+        "No schooling",
+        "Primary (Grades 1–5)",
+        "Secondary (Up to O/L)",
+        "Higher (A/L or above)"
+    ])
+fedu=st.selectbox("Father's educatonal level",[
+        "No schooling",
+        "Primary (Grades 1–5)",
+        "Secondary (Up to O/L)",
+        "Higher (A/L or above)"
+    ])
 
 st.subheader("Academic Performance")
+
 
 
 
@@ -33,6 +49,19 @@ st.subheader("Learning Environment")
 studytime_label=st.selectbox("Weekly Study Time(Outside the school hours)",["< 2 hours","2-5 hours","5-10 hours",">10 hours"])
 
 internet_label=st.radio("Internet at home",["Yes","No"])
+
+goout=st.slider("Frequency of going out with freinds",min_value=1,max_value=5,value=3,help="1=very low ,5=very high")
+
+freetime=st.slider("Amout of free time after school",min_value=1,max_value=5,value=3)
+
+health=st.slider("Current health status",min_value=1,max_value=5,value=3,help="1=very poor ,5=Excellent")
+
+edu_map = {
+    "No schooling": 0,
+    "Primary (Grades 1–5)": 1,
+    "Secondary (Up to O/L)": 2,
+    "Higher (A/L or above)": 3
+}
 
 
 sex_map = {"Male": 0, "Female": 1}
@@ -49,14 +78,17 @@ inputs={
     "absences":absences,
     "studytime":studytime_map[studytime_label],
     "internet":internet_map[internet_label],
+    "medu":edu_map[medu],
+    "fedu":edu_map[fedu],
+    "goout":goout,
+    "health":health,
+    "freetime":freetime,
 }
 DEFAULTS={
     "school": 0,
     "address": 1,
     "famsize": 1,
     "Pstatus": 1,
-    "Medu": 2,
-    "Fedu": 2,
     "Mjob": 3,
     "Fjob": 3,
     "reason": 0,
@@ -70,11 +102,8 @@ DEFAULTS={
     "higher": 1,
     "romantic": 0,
     "famrel": 3,
-    "freetime": 3,
-    "goout": 3,
     "Dalc": 1,
     "Walc": 1,
-    "health": 3
 }
 
 for feature in features:
